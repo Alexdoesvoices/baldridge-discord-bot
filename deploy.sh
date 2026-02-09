18f5b501d53f05f7d69dfe6a9e1a459e54bad16f
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2026-02-08 19:08:49 -0600

    added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!usr/bim/env bash
+
+git fetch origin master
+git reset --hard origin/master
+git clean -fd
+
+chmod +x deploy.sh
+
+bun install
+
+pm2 restart ecosystem.config.cjs
+
+echo "Done!"
